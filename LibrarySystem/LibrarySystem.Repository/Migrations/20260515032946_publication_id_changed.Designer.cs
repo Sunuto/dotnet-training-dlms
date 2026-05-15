7204a3182b2239206674605cb53cfa37912de741
Anu Thakur <anuthakur2062@gmail.com>
2026-05-15 09:16:02 +0545

    publication migration file

--- LibrarySystem/LibrarySystem.Repository/Migrations/20260515032946_publication_id_changed.Designer.cs
+++ LibrarySystem/LibrarySystem.Repository/Migrations/20260515032946_publication_id_changed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LibrarySystem.Repository.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibrarySystem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260515032946_publication_id_changed")]
+    partial class publication_id_changed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.7");
@@ -167,7 +170,7 @@ namespace LibrarySystem.Migrations
 
             modelBuilder.Entity("LibrarySystem.Repository.Models.LibraryLocation", b =>
                 {
-                    b.Property<int>("LocationId")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
 
@@ -203,7 +206,7 @@ namespace LibrarySystem.Migrations
                     b.Property<string>("Status")
                         .HasColumnType("TEXT");
 
-                    b.HasKey("LocationId");
+                    b.HasKey("Id");
 
                     b.ToTable("LibraryLocations");
                 });
