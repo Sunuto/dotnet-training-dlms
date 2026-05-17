4f5d99d0458fba12a8cd72d16167e9beb3d370af
Rashik Tuladhar <rashik-tuladhar>
2026-05-17 07:53:18 +0545

    minor changes in ui, middle name validation removed

--- LibrarySystem/LibrarySystem.Shared/AuthorData/AuthorDetails.cs
+++ LibrarySystem/LibrarySystem.Shared/AuthorData/AuthorDetails.cs
@@ -7,7 +7,6 @@ namespace LibrarySystem.Shared.AuthorData
         public int AuthorId { get; set; }
         [Required(ErrorMessage = "Please enter the name field.")]
         public string? FirstName { get; set; }
-        [Required]
         public string? MiddleName { get; set; }
         [Required]
         public string? LastName { get; set; }
